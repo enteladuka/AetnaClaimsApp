585eeee04746ac0756f453933132e886266a96fd
Entela Duka <enteladuka@gmail.com>
2018-12-30 21:36:22 -0500

    Removed rx number from data

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -3,7 +3,9 @@ class Claim < ApplicationRecord
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       row.to_hash
-      Claim.create(:claim_date => row["Date"], :pharmacy => row["Serviced By/Prescription Number"], :drug => row["Drug Name"], :prescription_cost => row[" Prescription Cost"], :plan_paid => row["Plan Paid"], :patient_paid => row["Your Responsibility"])
+      pharmacy = row["Serviced By/Prescription Number"].rstrip #removes extra space at the end of string
+      pharmacy[/[R][X]\d+$/] = '' #removes rxnumber/regex proof: https://rubular.com/r/FinC8OSIqR40
+      Claim.create(:claim_date => row["Date"], :pharmacy => pharmacy, :drug => row["Drug Name"], :prescription_cost => row[" Prescription Cost"], :plan_paid => row["Plan Paid"], :patient_paid => row["Your Responsibility"])
     end
   end
 end
