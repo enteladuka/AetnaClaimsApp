04bba738a14fd73de3fdf03151905ff58940b8ba
Entela Duka <enteladuka@gmail.com>
2019-01-02 03:03:41 -0500

    Minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'claims#new', as: 'SubmitClaims'
+  root 'claims#new'
 
   resources :claims, only: [:new, :create, :index] do
     collection { post :import }
   end
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
